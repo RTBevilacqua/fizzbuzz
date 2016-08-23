30a1edc02ffa0e3c2053b57f03056b17b1be6a16
RTBevilacqua <rtb983@yahoo.com>
2016-08-23 10:04:01 -0400

    new fizzbuzz

--- script.js
+++ script.js
@@ -2,21 +2,33 @@
 // divisible by 5 = buzz
 // divisible by 3 & 5 = fizz buzz
 
+$(document).ready(function(){
+
+//prompt to supply number
+var question = (prompt('Guess a number that is divisible by 3, 5, or both!'));
+var hat = parseInt(question);
+
+//function takes integer as argument.
+
+// function counts from one to argument value
 
 
-for (num = 0; num < 101; num++) {
-	if (num % 15 == 0) {
+//function substitutes 'fizz' 'buzz' 'fizzbuzz'
+
+	if (hat % 15 == 0) {
 	document.write("fizzbuzz" + '<br>');
 	}
-	else if ( num % 5 == 0) {
+	else if ( hat % 5 == 0) {
 		document.write("buzz" + '<br>');
 	}
-	else if (num % 3 == 0) {
+	else if (hat % 3 == 0) {
 		document.write("fizz" + '<br>');
 	}
-	else {
-		document.write(num + '<br>');
+	else if (isNaN(hat)) {
+		alert('Please refresh and enter a number.');
 	}
-};
-
+	else {
+		document.write(hat + '<br>');
+	};
+});
 
